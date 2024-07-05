dfb78a1300231d2d0638a0141b8ae13d8cf36213
Jorge-Rmz <71052252+Jorge-Rmz@users.noreply.github.com>
2024-07-05 15:12:07 -0600

    page 2

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import streamlit as st
 import Funtions.functions as funct
 
 todos = funct.get_todos()
-
+st.set_page_config(layout="wide")
 
 def add_todo():
     todo_local = st.session_state["new_todo"] + "\n"
@@ -12,7 +12,8 @@ def add_todo():
 st.title("My todo App")
 
 st.subheader("This is my todo app. ")
-st.text("This app is to increase your productivity.")
+st.write("This app is to increase your <b>productivity</b>.",
+        unsafe_allow_html=True)
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -25,4 +26,4 @@ for index, todo in enumerate(todos):
 st.text_input(label="Enter a todo ", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
 
-st.session_state
+#st.session_state
